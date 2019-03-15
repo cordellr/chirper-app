e4d9cf4f493911c93f0aa8f8f62cf4f2c995ec5a
cordellr <cordell.rainock@gmail.com>
2019-03-14 19:53:07 -0500

    New chirp successfully appearing

--- src/components/inputForm.jsx
+++ src/components/inputForm.jsx
@@ -1,29 +1,37 @@
 import React from "react";
 
 const InputForm = props => {
-  const { onAuthorChange, onMessageChange, newAuthor, newMessageText } = props;
+  const {
+    onAuthorChange,
+    onMessageChange,
+    // newAuthor,
+    // newMessageText,
+    handleSubmit
+  } = props;
 
   return (
     <form>
       <div className="form-group m-2" style={{ maxWidth: "12rem" }}>
         <input
+          id="nameField"
           type="text"
           className="form-control"
           placeholder="Name"
-          value={newAuthor}
+          //   value={newAuthor}
           onChange={event => onAuthorChange(event.target.value)}
         />
       </div>
       <div className="form-group m-2" style={{ maxWidth: "24rem" }}>
         <textarea
+          id="messageField"
           className="form-control"
           placeholder="Chirp text..."
           rows="3"
-          value={newMessageText}
+          //   value={newMessageText}
           onChange={event => onMessageChange(event.target.value)}
         />
       </div>
-      <button type="submit" className="btn btn-primary">
+      <button type="button" onClick={handleSubmit} className="btn btn-primary">
         Submit
       </button>
     </form>
--- src/components/messageContainer.jsx
+++ src/components/messageContainer.jsx
@@ -6,39 +6,56 @@ class MessageContainer extends Component {
   state = {
     messages: [
       {
-        id: "1",
+        id: 1,
         author: "Joe Schmo",
         messageText:
           "This is chirp one. It goes on for a while--inserts some random commentary here--and then stops."
       },
       {
-        id: "2",
+        id: 2,
         author: "Jane Doe",
         messageText:
           "This is chirp number two. It will continue to expound about nothing, and then trail off into..."
       },
       {
-        id: "3",
+        id: 3,
         author: "Jack Black",
         messageText:
           "This is the third chirp, but it is not the least of chirps. New user, what do you have to say about all of this?"
       }
     ],
 
-    newAuthor: "",
-    newMessageText: ""
+    id: 4,
+    author: "",
+    messageText: ""
   };
 
   //the two methods below take the target values on change
-  //and set them as the state values.
+  //from inputForm and set them as the state values.
   handleAuthorChange = author => {
-    this.setState({ newAuthor: author });
-    console.log(this.state.newAuthor);
+    this.setState({ author });
+    console.log(this.state.author);
   };
 
   handleMessageChange = messageText => {
-    this.setState({ newMessageText: messageText });
-    console.log(this.state.newMessageText);
+    this.setState({ messageText });
+    console.log(this.state.messageText);
+  };
+
+  //This method looks confusing, but it's simply
+  //cloning the original messages array in state with the spread operator
+  //and adding a new object with the values entered in the form.
+  handleSubmit = () => {
+    this.setState({
+      messages: [
+        ...this.state.messages,
+        {
+          id: this.state.id,
+          author: this.state.author,
+          messageText: this.state.messageText
+        }
+      ]
+    });
   };
 
   render() {
@@ -47,8 +64,9 @@ class MessageContainer extends Component {
         <InputForm
           onAuthorChange={this.handleAuthorChange}
           onMessageChange={this.handleMessageChange}
-          author={this.state.newAuthor}
-          messageText={this.state.newMessageText}
+          //   author={this.state.author}
+          //   messageText={this.state.messageText}
+          handleSubmit={this.handleSubmit}
         />
         <MessageBox messages={this.state.messages} />
       </div>
